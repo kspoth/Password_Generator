e7caca06ce264f434e819fb2f16fd27a3a46bfc0
Kevin Spoth <kspoth@richmond.edu>
2020-12-07 23:05:15 -0500

    image folder for readme

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+//Random Password JS code
 var generateBtn = document.querySelector("#generate");
 
 
-// variable arrays for the characters that will go in the generated password.//
+// variable arrays for the characters that will go in the generated password.
 var loCase = "abcdefghijklmnopqrstuvwxyz";
 var upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var num = "0123456789";
@@ -10,25 +11,27 @@ var spChar = "!()-.?[]_`~;:!@#$%^&*+=";
 // function that will write the password.
 function writePassword() {
 
-  // After hitting the button "generate"
+  // After hitting the button "generate password" button the prompt will ask a series of questions.
   var length = Number(
     prompt("Please choose a number from 8-128")
   )
     if (length < 8) {
-      alert('Try Again, please choose a number between 8 and 128');
+      alert("Try Again, please choose a number higher than 8");
       return;
     };
     if (length > 128) {
-      alert('Try Again, please choose a number between 8 and 128');
+      alert("Try Again, please choose a number lower than 128");
       return;
     }
-  var lowerCase = confirm("Would you like lower case letters?");
-  var upperCase = confirm("Would you like upper case letters?");
-  var numbers = confirm("Do you want to include numbers?");
-  var specialChars = confirm("Do you want to include special characters?");
 
- 
-  var characterSet = "";
+//variables for questions
+var lowerCase = confirm("Would you like lower case letters?");
+var upperCase = confirm("Would you like upper case letters?");
+var numbers = confirm("Would you like numbers?");
+var specialChars = confirm("Would you like special characters?");
+
+  //Runs through prompt questions and will give characters if selected by the user.
+  var characters = "";
 
   if (lowerCase === true) {
     characters += loCase;
@@ -47,17 +50,17 @@ function writePassword() {
   }
 
   if (characters.length == 0) {
-    alert("Please select at least one character set");
+    alert("Error. Must select atleast one character set");
     return;
   }
 
   splitChar = characters.split("");
  
-  
+  //This function that reads the character string and generates the password based on length and user selections
   function generatePassword() {
     var pass = "";
     for (var i = 0; i < length; i++) {
-    pass += characterSet.charAt[(Math.floor(Math.random() * characterSet.length))];
+    pass += splitChar[(Math.floor(Math.random() * splitChar.length))];
   }
   return pass;
 }
@@ -71,4 +74,5 @@ var password = generatePassword();
 
 }
 
+//Event listener to generate button
 generateBtn.addEventListener("click", writePassword);
